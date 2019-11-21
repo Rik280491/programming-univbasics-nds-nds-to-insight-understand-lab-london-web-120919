71f5c27aba0a13deb477d226b866e9d86e71213f
Rikesh Balakrishnan <rik280491@googlemail.com>
2019-11-21 19:16:58 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -7,9 +7,5 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  row_index = 0
-  column_index = 0 
-  while column_index < directors_database.length do
-    puts directors_database[row_index][column_index]
-  end 
+ 
   
